2fcde321cea97b81aeb337ca8d31f9e35d016c9a
ShahnawazDev <93546124+ShahnawazDev@users.noreply.github.com>
2022-11-04 19:41:37 +0530

    Added all the changes required to run unity

--- app/src/main/java/com/example/cquant/MapsActivity.java
+++ app/src/main/java/com/example/cquant/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.cquant;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,6 +31,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button ar = findViewById(R.id.AR);
+        ar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(),UnityActivityHost.class);
+                startActivity(intent2);
+            }
+        });
     }
 
     /**
